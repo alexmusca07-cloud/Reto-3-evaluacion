78995e4ccb4f5bde1a23841868c862f219f530f9
1DAM <1DAM@A11-PC14>
2026-05-13 13:14:58 +0200

    ClienteDAO

--- Reto3Ev/src/dao/EmpleadoDAO.java
+++ Reto3Ev/src/dao/EmpleadoDAO.java
@@ -1,5 +1,39 @@
 package dao;
 
-public class EmpleadoDAO {
+import java.util.List;
+
+import modelo.Empleado;
+
+public class EmpleadoDAO implements GenericDAO<Empleado>{
+
+	@Override
+	public boolean insertar(Empleado objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Empleado> obtenerTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Empleado obtenerPorId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean actualizar(Empleado objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean eliminar(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
