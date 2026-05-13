7e2a8aaa7b51445d803a482e661d32f115346648
1DAM <1DAM@A11-PC14>
2026-05-13 13:22:51 +0200

    ClienteDAO

--- Reto3Ev/src/dao/ClienteDAO.java
+++ Reto3Ev/src/dao/ClienteDAO.java
@@ -1,8 +1,12 @@
 package dao;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import modelo.Cliente;
+import modelo.Factura;
+import util.ConexionBD;
 
 public class ClienteDAO implements GenericDAO<Cliente>{
 
@@ -14,10 +18,23 @@ public class ClienteDAO implements GenericDAO<Cliente>{
 
 	@Override
 	public List<Cliente> obtenerTodos() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		List<Cliente> clientes = new ArrayList<>();
+		String sql = "SELECT id, nombre, nacionalidad FROM autor ORDER BY nombre";
 
+		try (Connection conn = ConexionBD.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				ResultSet rs = pstmt.executeQuery()) {
+
+			while (rs.next()) {
+				clientes.add(mapearFila(rs));
+
+			}
+
+		} catch (SQLException e) {
+			System.err.println("Error SQL al obtener todos los clientes: " + e.getMessage());
+		}
+		return clientes;
+	}
 	@Override
 	public Cliente obtenerPorId(int id) {
 		// TODO Auto-generated method stub
@@ -34,6 +51,13 @@ public class ClienteDAO implements GenericDAO<Cliente>{
 	public boolean eliminar(int id) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	private Cliente mapearFila(ResultSet rs) throws SQLException{
+		Cliente c = new Cliente();
+		c.setId(rs.getInt("id"));
+		c.setDireccion(rs.getString("direccion"));
+		return c;
+
 	}
 
 }
