1595c09f9f82f57cd620e532caeb2a178b8423b5
1DAM <1DAM@A11-PC14>
2026-05-13 12:52:18 +0200

    prueba

--- Reto3Ev/src/app/Main.java
+++ Reto3Ev/src/app/Main.java
@@ -1,10 +1,23 @@
 package app;
 
+import java.sql.Connection;
+
+import util.ConexionBD;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+				try (Connection con = ConexionBD.getConnection()) {
+					if (con != null) {
+						System.out.println("CONEXIÓN EXITOSA");
+					} else {
+						System.out.println("Fallo Conexión");
+					}
+				} catch (Exception e) {
+					System.out.println("Error: " + e.getMessage());
 
-}
+				}
+			}
+		}
+
