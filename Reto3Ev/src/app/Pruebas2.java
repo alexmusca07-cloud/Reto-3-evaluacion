d67428e94ba19a9c59a1d83fee31367b261e9eed
1DAM <1DAM@A11-PC14>
2026-05-19 11:51:18 +0200

    EJ15

--- Reto3Ev/src/app/Pruebas2.java
+++ Reto3Ev/src/app/Pruebas2.java
@@ -1,20 +1,36 @@
 package app;
 
-import dao.ClienteDAO;
-import modelo.Cliente;
-import modelo.ClienteFichero;
+import dao.*;
+import modelo.*;
 
 public class Pruebas2 {
 
-	
 	public static void main(String[] args) {
 		ClienteDAO cdao = new ClienteDAO();
-		/*case 6:
-		cdao.insertar(new Cliente("12345678V", "Pepe Carrera", "Plaza Mozart"));
-		break;
-		*/
-		ClienteFichero.guardarFichero(cdao.obtenerTodos());
-		ClienteFichero.leerFichero();
+		EmpleadoDAO edao = new EmpleadoDAO();
+		FacturaDAO fdao = new FacturaDAO();
+		LineaFacturaDAO ldao = new LineaFacturaDAO();
+		ProductoDAO pdao = new ProductoDAO();
+		/*
+		 * case 6: cdao.insertar(new Cliente("12345678V", "Pepe Carrera",
+		 * "Plaza Mozart")); break;
+		 * 
+		 * ClienteFichero.guardarFichero(cdao.obtenerTodos());
+		 * ClienteFichero.leerFichero();
+		 */
+		// case 15:
+		int contador = 0;
+		double sum = 0;
+		for (Empleado e : edao.obtenerTodos()) {
+			for (Factura f : fdao.obtenerPorMesEIdEmpleado(5, e.getId())) {
+				sum += f.getTotal();
+				contador++;
+			}
+			System.out.println("Empleado de id " + e.getId() + " ha hecho " + contador + "facturas  y ha facturado " + sum + "€");
+			contador=0;
+			sum=0;
+		}
+
 	}
 
 }
